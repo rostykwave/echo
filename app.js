3b984d3b8ab1067bb1cf79682380feefd7544e30
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-03-19 13:57:25 +0200

    1

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
-let ivs = 1.1
+// let ivs = 1.1
 
 
-if (ivs >1.1) {
-    console.log('Гіпертрофія МШП')
-} else {
-    console.log('Норма')
-}
+// if (ivs >1.1) {
+//     console.log('Гіпертрофія МШП')
+// } else {
+//     console.log('Норма')
+// }
+
+// const ivs = document.querySelector('#ivs.textContent')
+// console.log(ivs)
+
+// if (ivs >1.1) {
+//     console.log('Гіпертрофія МШП')
+// } else {
+//     console.log('Норма')
+// }
