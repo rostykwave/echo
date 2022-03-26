f66027264fb3b1bb9526f772fb1a021f0e1bacfa
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-03-26 16:44:55 +0200

    la

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ function calculateEcho() {
 
     // ivs,lvWall
     const rv = parseFloat(document.querySelector("#rv").value);
+    const la = parseFloat(document.querySelector("#la").value);
     const ivs = parseFloat(document.querySelector("#ivs").value);
     const lvWall = parseFloat(document.querySelector("#lvWall").value);
 
     let result = document.querySelector("#result");
     let rvResult;
+    let laResult;
     let wallsResult;
     let norma;
 
@@ -23,48 +25,37 @@ function calculateEcho() {
         rvResult = '';
     }
 
+    //Умова для ЛП
+    if (la > 4) {
+       laResult = `Дилятація лівого передсердя. `
+    } else {
+        laResult = '';
+    }
+
     // Умови для МШП і ЗСЛШ
     if (ivs > 1.1 && lvWall > 1.1) {
-        wallsResult = `Гіпертрофія стінок ЛШ. `;
+        wallsResult = `Гіпертрофія стінок лівого шлуночка. `;
 } else if (ivs > 1.1) {
-    wallsResult = `Гіпертрофія МШП. `;
+    wallsResult = `Гіпертрофія міжшлуночкової перегородки. `;
     } else if (lvWall > 1.1){
-        wallsResult = `Гіпертрофія задньої стінки ЛШ. `;
+        wallsResult = `Гіпертрофія задньої стінки лівого шлуночка. `;
     } else {
         wallsResult = '';
     }
 
     // Розміри камер серця в межах норми
     if (rvResult === '' && wallsResult === '') {
-        norma = 'Розміри камер серця в межах норми';
+        norma = 'Розміри камер серця в межах норми. ';
     } else {
         norma = '';
     }
 
     ///Виведення інформації
-    result.innerHTML = rvResult + wallsResult + norma ;
+    result.innerHTML = rvResult + laResult + wallsResult + norma ;
 }
 
 
 ///Завдання:
 //зв'язати всі результати по інпутам в один і вивести в одне вікно
 ///зробити перевірку на введення null і undefined, букв, ком між числами
-
-// let ivs = 1.1
-
-
-// if (ivs >1.1) {
-//     console.log('Гіпертрофія МШП')
-// } else {
-//     console.log('Норма')
-// }
-
-// const ivs = document.querySelector('#ivs.textContent')
-// console.log(ivs)
-
-// if (ivs >1.1) {
-//     console.log('Гіпертрофія МШП')
-// } else {
-//     console.log('Норма')
-// }
 
