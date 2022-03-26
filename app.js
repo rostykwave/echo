25d3cbbb216cf6789f99faa1b84622ca41d54e67
Rostyslav Kovalchuk <rostykwave@GMAIL.COM>
2022-03-26 16:23:48 +0200

    rv

--- app.js
+++ app.js
@@ -7,22 +7,48 @@ window.onload = () => {
 function calculateEcho() {
 
     // ivs,lvWall
-    let ivs = parseFloat(document.querySelector("#ivs").value);
-    let lvWall = parseFloat(document.querySelector("#lvWall").value);
+    const rv = parseFloat(document.querySelector("#rv").value);
+    const ivs = parseFloat(document.querySelector("#ivs").value);
+    const lvWall = parseFloat(document.querySelector("#lvWall").value);
 
     let result = document.querySelector("#result");
+    let rvResult;
+    let wallsResult;
+    let norma;
 
-    if (ivs > 1.1 && lvWall > 1.1) {
-        result.innerHTML = `Гіпертрофія стінок ЛШ`;
-} else if (ivs > 1.1 && lvWall <= 1.1) {
-    result.innerHTML = `Гіпертрофія МШП`;
-    } else if (ivs <= 1.1 && lvWall > 1.1){
-        result.innerHTML = `Гіпертрофія задньої стінки ЛШ`;
+    //Умова для ПШ
+    if (rv > 2.6) {
+       rvResult = `Дилятація правого шлуночка`
     } else {
-        result.innerHTML = `Норма`;
+        rvResult = '';
     }
+
+    // Умови для МШП і ЗСЛШ
+    if (ivs > 1.1 && lvWall > 1.1) {
+        wallsResult = `Гіпертрофія стінок ЛШ. `;
+} else if (ivs > 1.1 && lvWall <= 1.1) {
+    wallsResult = `Гіпертрофія МШП. `;
+    } else if (ivs <= 1.1 && lvWall > 1.1){
+        wallsResult = `Гіпертрофія задньої стінки ЛШ. `;
+    } else {
+        wallsResult = '';
+    }
+
+    // Розміри камер серця в межах норми
+    if (rvResult === '' && wallsResult === '') {
+        norma = 'Розміри камер серця в межах норми';
+    } else {
+        norma = '';
+    }
+
+    ///Виведення інформації
+    result.innerHTML = rvResult + wallsResult + norma ;
 }
 
+
+///Завдання:
+//зв'язати всі результати по інпутам в один і вивести в одне вікно
+///зробити перевірку на введення null і undefined, букв, ком між числами
 
 // let ivs = 1.1
 
